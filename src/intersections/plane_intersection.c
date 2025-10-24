d709bcfacb886ce85d2271ae122cfe0f9985f619
Tetiana Cherniaieva <tetiana.cherni@gmail.com>
2025-10-24 15:42:38 +0000

    refactor: Make proper folder name

--- src/intersections/plane_intersection.c
+++ src/intersections/plane_intersection.c
@@ -14,7 +14,7 @@
 */
 
 /* function check is there intersection by using bool and setup valu in t */
-bool	hit_plane(t_ray *с_ray, t_plane *plane, float *t)// change to doubles!!!better foor math calculation and preciese rendering
+bool	hit_plane(t_ray *c_ray, t_plane *plane, float *t)// change to doubles!!!better foor math calculation and preciese rendering
 {
 	float	num;
 
