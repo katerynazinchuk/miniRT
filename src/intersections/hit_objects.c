d9659f9bbb4ecb33efad804d078328a4862475cd
Kateryna Zinchuk <zinchuck.k@gmail.com>
2025-11-17 14:51:49 +0100

    Add layered ray hit handling for multiple objects
    Support full layering for:
    - multiple objects of the same type
    - multiple objects of different types
    
    The hit logic now finds the minimal t for each ray.

--- src/intersections/hit_objects.c
+++ src/intersections/hit_objects.c
@@ -6,7 +6,7 @@
 /*   By: kzinchuk <kzinchuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 12:13:30 by kzinchuk          #+#    #+#             */
-/*   Updated: 2025/11/17 14:39:23 by kzinchuk         ###   ########.fr       */
+/*   Updated: 2025/11/17 14:49:05 by kzinchuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,56 @@
 
 bool hit_all_spheres(t_ray *c_ray, t_scene *scene, t_hit_rec *hit_rec)
 {
+	bool hit_any = false;
+	double closest_t = T_MAX;
+	t_hit_rec hit;
+	int i;
 	
-	return true;
+	i = 0;
+	
+	while(i < scene->objects.sp_count)
+	{
+		if(hit_sphere(c_ray, &scene->objects.sps[i], &hit))
+		{
+			if(hit.t < closest_t && hit.t > T_MIN)
+			{
+				hit_any = true;
+				closest_t = hit.t;
+				*hit_rec = hit;
+				hit_rec->type = OBJ_PLANE;
+				hit_rec->index = i;
+			}
+		}
+		i++;
+	}
+	return (hit_any);
 }
 
 bool hit_all_planes(t_ray *c_ray, t_scene *scene, t_hit_rec *hit_rec)
 {
+	bool hit_any = false;
+	double closest_t = T_MAX;
+	t_hit_rec hit;
+	int i;
 	
-	return true;
+	i = 0;
+	
+	while(i < scene->objects.pl_count)
+	{
+		if(hit_cylinder(c_ray, &scene->objects.pls[i], &hit))
+		{
+			if(hit.t < closest_t && hit.t > T_MIN)
+			{
+				hit_any = true;
+				closest_t = hit.t;
+				*hit_rec = hit;
+				hit_rec->type = OBJ_PLANE;
+				hit_rec->index = i;
+			}
+		}
+		i++;
+	}
+	return (hit_any);
 }
 
 bool hit_all_cylinders(t_ray *c_ray, t_scene *scene, t_hit_rec *hit_rec)
@@ -33,9 +75,9 @@ bool hit_all_cylinders(t_ray *c_ray, t_scene *scene, t_hit_rec *hit_rec)
 	
 	i = 0;
 	
-	while(i < scene->objects.cyl_count)
+	while(i < scene->objects.cy_count)
 	{
-		if(hit_cylinder(c_ray, &scene->objects.cyl[i], &hit))
+		if(hit_cylinder(c_ray, &scene->objects.cys[i], &hit))
 		{
 			if(hit.t < closest_t && hit.t > T_MIN)
 			{
