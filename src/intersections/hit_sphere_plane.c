3f4623303b3554b6292c33abd00cfd29a57cede3
tetiana-cherni <tetiana.cherni@gmail.com>
2025-12-17 13:37:29 +0100

    refactor: adapt hit_sphere_plane.c for norminette

--- src/intersections/hit_sphere_plane.c
+++ src/intersections/hit_sphere_plane.c
@@ -5,35 +5,40 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tchernia <tchernia@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/03 15:42:59 by kzinchuk          #+#    #+#             */
-/*   Updated: 2025/12/17 13:07:42 by tchernia         ###   ########.fr       */
+/*   Created: 2025/12/17 13:32:39 by tchernia          #+#    #+#             */
+/*   Updated: 2025/12/17 13:36:57 by tchernia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
 
-bool	hit_sphere(const t_ray *c_ray, const t_sphere *sphere, t_hit *hit_rec)
+bool	hit_sphere(const t_ray *c_ray, const t_sphere *sphere, t_hit *hit)
 {
-	t_vec oc = vec_sub(c_ray->origin, sphere->center);
-	double a = vec_dot(c_ray->dir, c_ray->dir); //(D·D); c_ray->direction * c_ray->direction
-	double half_b = vec_dot(oc, c_ray->dir);//oc * c_ray->direction;
-	double c = vec_dot(oc, oc) - (sphere->radius * sphere->radius);//oc * oc - sphere->radius * sphere->radius;
-	double disc = (half_b * half_b) - (a * c);
-	if(disc < 0)
-		return false;
-	//var for getting root. result of moving elements to the right to find clean t from squere equesion
-	hit_rec->t = (-half_b - sqrt(disc)) / a;//first cross cos it always smaller
-	if(hit_rec->t < T_MIN || hit_rec->t > T_MAX)
+	t_vec	oc;
+	double	a;
+	double	half_b;
+	double	c;
+	double	disc;
+
+	oc = vec_sub(c_ray->origin, sphere->center);
+	a = vec_dot(c_ray->dir, c_ray->dir);
+	half_b = vec_dot(oc, c_ray->dir);
+	c = vec_dot(oc, oc) - (sphere->radius * sphere->radius);
+	disc = (half_b * half_b) - (a * c);
+	if (disc < 0)
+		return (false);
+	hit->t = (-half_b - sqrt(disc)) / a;
+	if (hit->t < T_MIN || hit->t > T_MAX)
 	{
-		hit_rec->t = (-half_b + sqrt(disc)) / a;//second cross cos it bigger
-		if(hit_rec->t < T_MIN || hit_rec->t > T_MAX)
-			return false;
+		hit->t = (-half_b + sqrt(disc)) / a;
+		if (hit->t < T_MIN || hit->t > T_MAX)
+			return (false);
 	}
-	hit_rec->intersection = vec_add(c_ray->origin, vec_scale(c_ray->dir, hit_rec->t));
-	hit_rec->normal = vec_sub(hit_rec->intersection, sphere->center);
-	hit_rec->normal = vec_normalize(hit_rec->normal);
-	hit_rec->color = sphere->color;
-	return true;
+	hit->intersection = vec_add(c_ray->origin, vec_scale(c_ray->dir, hit->t));
+	hit->normal = vec_sub(hit->intersection, sphere->center);
+	hit->normal = vec_normalize(hit->normal);
+	hit->color = sphere->color;
+	return (true);
 }
 
 bool	hit_plane(const t_ray *c_ray, const t_plane *plane, t_hit *hit_rec)
